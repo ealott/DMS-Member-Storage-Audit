d990ac5d360f9c52c5a74fcf771a903c3eb31140
Evan Lott <ealott@gmail.com>
2017-10-06 06:33:56 -0500

    added basic label printing for inactive bins

--- member_storage_audit.py
+++ member_storage_audit.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python
 
-import requests,json,zebra
+import requests,json,socket
+from time import gmtime, strftime
+
+
+current_time = strftime("%a, %d %b %Y %X +0000", gmtime())
+print (current_time)
+
+#network settings for zebra LP 2844 printer (supports EPL only)
+TCP_IP = '192.168.204.222'
+TCP_PORT = 9100
+BUFFER_SIZE = 1024
 
 
 qr_data = (input('Please scan QR code: ')).strip().split(',')
 
 rfid = ''.join(qr_data[1:2])
+firstName = ''.join(qr_data[4:5])
+lastName = ''.join(qr_data[5:])
+email = ''.join(qr_data[3:4])
+phone = ''.join(qr_data[2:3])
 
 print("Member's RFID: " + rfid)
 
@@ -20,21 +34,41 @@ response = requests.request("POST", url, data=payload, headers=headers)
 
 response_body = json.loads(response.text)
 
+#print(response.text)
 #print(response_body)
 
 
 member_active = response_body['result']['accessGranted']
 
-if member_active:
+
+if member_active: 
     print("Member active")
     
-else:
+else: #test data for inactive member: dms,00072497335,1972123456789,test@gmail.com,John,Smith
     print("Member not active, or data lookup failed.")
-    print ("Member name: " + response_body['result']['user']['fullName'])
-    
-label = ""
+    print("Member name: " + firstName + " " + lastName)
+    print("Member email: " + email)
+    print("Member phone: " + phone)
 
-#https://pypi.python.org/pypi/zebra/
-#pip-3.2 install zebra
-z = zebra('zebra_test')
-z.output(label)
+    #note that the LP 2844 only supports EPL, not ZPL!
+    #EPL references: https://en.wikipedia.org/wiki/Eltron_Programming_Language https://www.zebra.com/content/dam/zebra/manuals/en-us/printer/epl2-pm-en.pdf
+    #Ah_pos,v_pos,rotation,font,h_mult,v_mult,reverse,"text" - ASCII text
+
+    epl = (
+    "N" "\n"
+    'A0,0,0,5,1,1,N,"BIN OWNER INACTIVE"' "\n"
+    'A0,75,0,4,1,1,N,"SCAN DATE: '  + str(current_time) + '"\n'
+    'A0,150,0,4,1,1,N,"MEMBER INFO:"' "\n"
+    'A0,200,0,3,1,1,N,"NAME: '  + str(firstName + " " + lastName) + '"\n'
+    'A0,300,0,3,1,1,N,"EMAIL: '  + str(email) + '"\n'
+    'A0,400,0,3,1,1,N,"PHONE: '  + str(phone) + '"\n'
+    "LO,600,750,20" "\n"
+    'B50,800,0,3,3,7,200,B,"' + str(rfid) + '"\n'
+    "P1" "\n"
+    )
+
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send(bytes(epl, "utf-8"))
+    s.close()
